80e8ca93f8752d8b7d274da861a59f698449c2f0
Ishwant Singh <ishwant8@gmail.com>
2019-04-03 17:20:48 +0530

    userRoutes requests made

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,16 +2,67 @@ const express = require("express");
 const userRoute = express.Router();
 const userDb = require("../data/helpers/userDb");
 
-routes.use(express.json());
+userRoute.use(express.json());
 
 userRoute.get("/", (req, res, next) => {
   userDb
     .get()
     .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+
+userRoute.get("/:id", (req, res, next) => {
+  userDb
+    .getById(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+userRoute.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  userDb
+    .update(id, { name: req.body.name })
+    .then(data => {
+      if (data == 1) {
+        res.status(200).json({ message: `User of id ${id} was updated` });
+      } else {
+        res.status(500).json("an error occured", err);
+      }
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+userRoute.post("/", (req, res, next) => {
+  userDb
+    .insert({ name: req.body.name })
+    .then(data => {
       res.json(data);
     })
     .catch(err => {
       res.json("an error occured", err);
+    });
+});
+userRoute.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  userDb
+    .remove(id)
+    .then(data => {
+      if (data == 1) {
+        res.status(200).json({ message: `User of id ${id} was deleted` });
+      } else {
+        res.status(500).json("an error occured", err);
+      }
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
     });
 });
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 //const md = rquire("./middleware")
 const postRoute = require("./routes/postRoutes");
-const userRoute = require("./routes/postRoutes");
+const userRoute = require("./routes/userRoutes");
 
 const server = express();
 
@@ -13,7 +13,7 @@ server.use("/api/posts", postRoute);
 server.use("/api/users", userRoute);
 
 server.get("/", (req, res, next) => {
-  res.send(`<h1>Welcome to the server land!</h1>`);
+  res.status(200).json(`<h1>Welcome to the server land!</h1>`);
 });
 
 module.exports = server;
