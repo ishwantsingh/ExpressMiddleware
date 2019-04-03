41918acf989f5210b93ad1e69d9f54519a8d27cd
Ishwant Singh <ishwant8@gmail.com>
2019-04-03 17:29:23 +0530

    postRoutes requests made

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,10 +8,61 @@ postRoute.get("/", (req, res, next) => {
   postDb
     .get()
     .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+
+postRoute.get("/:id", (req, res, next) => {
+  postDb
+    .getById(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+postRoute.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  postDb
+    .update(id, { text: req.body.text, user_id: req.body.user_id })
+    .then(data => {
+      if (data == 1) {
+        res.status(200).json({ message: `Post of id ${id} was updated` });
+      } else {
+        res.status(500).json("an error occured", err);
+      }
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
+    });
+});
+postRoute.post("/", (req, res, next) => {
+  postDb
+    .insert({ text: req.body.text, user_id: req.body.user_id })
+    .then(data => {
       res.json(data);
     })
     .catch(err => {
       res.json("an error occured", err);
+    });
+});
+postRoute.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  postDb
+    .remove(id)
+    .then(data => {
+      if (data == 1) {
+        res.status(200).json({ message: `Post of id ${id} was deleted` });
+      } else {
+        res.status(500).json("an error occured", err);
+      }
+    })
+    .catch(err => {
+      res.status(500).json("an error occured", err);
     });
 });
 
